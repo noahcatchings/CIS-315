d71d89905a2cb3dd2531ed1f4a8c3a0b27ace324
Noah Catchings <120297029+noahcatchings@users.noreply.github.com>
2025-01-28 21:12:08 -0600

    Update Contact.java
    Added condition to ensure phone number is correctly entered

--- Contact/src/Contact.java
+++ Contact/src/Contact.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 /** Noah Catchings
  * CIS-315
  * 01/27/2025
- * This program will...
+ * This program will prompt the user to enter their information and will ensure 
+ * the information is entered correctly.
  */
 
-public class Info {
+public class Contact {
 	public static void main(String[] args) {
 		
 		String lastname;
@@ -25,13 +26,28 @@ public class Info {
 		
 		System.out.println("Middle Initial: ");
 		String midInput = scnr.next();
-		midinit = midInput.toUpperCase().charAt(0);
+		midinit = midInput.charAt(0);
+		String midinitcheck = midInput.toUpperCase();
+		if (midinitcheck != midInput) {
+			System.out.println("ERROR: MUST BE CAPITAL");
+		}
+		
 		
 		System.out.println("Phone (555) 555-1212: ");
 		phone = scnr.next();
+		while (phone.length() < 12 || phone.charAt(0) != '(' || phone.charAt(4) != ')' || 
+				phone.charAt(8) != '-') {
+			
+			System.out.println("ERROR: INVALID FORMAT | PLEASE TRY AGAIN");
+			phone = scnr.next();
+		}
+		
+	
 
 		System.out.println("ID:\t 556");
-		System.out.println("Name: \t" + firstname + " " + midinit + " " + lastname);
+		System.out.println("Name:\t" + firstname + " " + midinit + " " + lastname);
+		System.out.println("Phone: " + phone);
 		
+		scnr.close();
 	}
 }
